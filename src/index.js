b5fcb5abe8305c9367f867370b5aaca2c40f6213
Nezih <20neziht@gmail.com>
2019-02-01 22:41:41 +0300

    Added react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-document.write('i am ready 111');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('root')
+);
