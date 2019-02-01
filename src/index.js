eb728d441b3cb6bdd7fb4677e3c35dbcd472320c
Nezih <20neziht@gmail.com>
2019-02-01 22:55:03 +0300

    check if hot object exists in module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = 'My Minimal React Webpack Babel Setup 11';
 
-ReactDOM.render(
-<div>{title}</div>,
-document.getElementById('root')
-);
+function App() {
+    return (
+        <div>{title}</div>
+    )
+}
 
-module.hot.accept();
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+if(module.hot){
+    module.hot.accept();
+}
